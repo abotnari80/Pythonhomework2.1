5c35993ba16c738664b50fec7881a292b0f04ec8
abotnari80 <abotnari80@gmail.com>
2022-07-27 09:42:37 +0300

    задача 4

--- Task4.py
+++ Task4.py
@@ -1,6 +1,17 @@
-n = int(input('Введите количество чисел в последовательности: '))
-sum = 0
-for i in range(0, n):
-    input_value = int(input(f'Введите число #{i}: '))
-    sum += input_value
-print('Сумма всех чисел последовательности:', sum)
+#1. Задайте список из N элементов, заполненных числами 
+#из промежутка [-N, N]. Найдите произведение элементов 
+#на указанных позициях. Позиции хранятся в файле file.txt 
+#в одной строке одно число.
+#2. Реализуйте алгоритм перемешивания списка.
+import random
+
+def newList(x):
+    list = [i for i in range(-x, x+1)]
+    return list
+def shuffleList(x):
+    random.shuffle(x)
+    return x
+n = int(input('Введите количество чисел последовательности: '))
+list = newList(n)
+answer = shuffleList(list)
+print(answer)
