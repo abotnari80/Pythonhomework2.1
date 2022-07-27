393ca84a58ae6a5e59a611cb431216350f8fa047
abotnari80 <abotnari80@gmail.com>
2022-07-27 09:19:07 +0300

    задача 1

--- Task.py
+++ Task.py
@@ -1,12 +1,16 @@
+#Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+def sumFloat(num):
+    x = num.split(".") 
+    a = int(x[0]) 
+    b = int(x[1])
+    sum = 0
+    while a != 0:
+        sum = sum + (a % 10)
+        a = a // 10
+    while b != 0:
+        sum = sum + (b % 10)
+        b = b // 10
+    return sum
 num = input("Введите число: ")
-x = num.split(".") 
-a = int(x[0]) 
-b = int(x[1])
-sum = 0
-while a != 0:
-    sum = sum + (a % 10)
-    a = a // 10
-while b != 0:
-    sum = sum + (b % 10)
-    b = b // 10
-print('Сумма равна: ', sum)  
+answer = sumFloat(num)       
+print('Сумма равна: ', answer)
