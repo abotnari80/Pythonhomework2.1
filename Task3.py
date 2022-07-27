902e02b6e3ad1eb376cb9a838f0110a823a704d0
abotnari80 <abotnari80@gmail.com>
2022-07-27 09:33:08 +0300

    задача 3

--- Task3.py
+++ Task3.py
@@ -1,4 +1,10 @@
-import random
-a =  ['Веселый пианист', 250, 3.14, 'True '] 
-random.shuffle(a)
-print(a)
+#Задайте список из n чисел последовательности (1 + 1/n)^n и выведите на экран их сумму.
+def foo(x):
+    sum = 0
+    for i in range(0, x+1):
+        x = (1 + 1/x)**x
+        sum += x
+    return sum
+n = int(input('Введите количество чисел в последовательности: '))
+answer = foo(n)
+print('Сумма всех чисел последовательности:', answer)
